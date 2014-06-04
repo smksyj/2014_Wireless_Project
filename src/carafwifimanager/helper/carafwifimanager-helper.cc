7b0163ae8bc700b8c087b01355d228d885c81698
smksyj <smksyj@gmail.com>
2014-06-04 20:20:36 +0900

    add other module folders

---
+++ src/carafwifimanager/helper/carafwifimanager-helper.cc
@@ -0,0 +1,11 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+
+#include "carafwifimanager-helper.h"
+
+namespace ns3 {
+
+/* ... */
+
+
+}
+
---
+++ src/carafwifimanager/helper/carafwifimanager-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef CARAFWIFIMANAGER_HELPER_H
+#define CARAFWIFIMANAGER_HELPER_H
+
+#include "ns3/carafwifimanager.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* CARAFWIFIMANAGER_HELPER_H */
+
